d171bbb3e474fef0a3c74b5e69d7914468248579
Alade Toheeb <aladetoheeb5@gmail.com>
2023-01-05 23:59:30 +0100

    Task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,7 @@
 """ Module 6-sum_mixed_list """
 
 
-def sum_mixed_list(mxd_lst: list[float]) -> float:
+def sum_mixed_list(mxd_lst: list[float, int]) -> float:
     """ Function that returns the sum of a list float """
     sum_i: float = 0
     for i in mxd_lst:
