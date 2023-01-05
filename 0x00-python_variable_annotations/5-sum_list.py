df615951f6ad6973dbe853a45fe085c96e08c9aa
Alade Toheeb <aladetoheeb5@gmail.com>
2023-01-05 23:35:20 +0100

    Task 4 and 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module 5-sum_list """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Function that returns the sum of a list """
+    return sum(input_list)
