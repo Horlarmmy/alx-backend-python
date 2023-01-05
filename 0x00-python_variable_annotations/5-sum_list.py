ad6fa67a31b0ef273dfe2b5cd1beca94fdd60914
Alade Toheeb <aladetoheeb5@gmail.com>
2023-01-05 23:49:28 +0100

    Refactor Task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,4 +4,7 @@
 
 def sum_list(input_list: list[float]) -> float:
     """ Function that returns the sum of a list float """
-    return sum(input_list)
+    sum_i: float = 0
+    for i in input_list:
+        sum_i += i
+    return sum_i
