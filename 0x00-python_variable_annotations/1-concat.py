77b259d4d88e918714faa8e67462ba4a0b17a5c5
Alade Toheeb <aladetoheeb5@gmail.com>
2023-01-05 23:10:43 +0100

    Task 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module 1-concat.py """
 
+
 def concat(str1: str, str2: str) -> str:
     """ Function that concatenates two strings """
     return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module 2-floor """
+
+
+def floor(n: float) -> int:
+    """ Function that that returns the floor of a float """
+    return int(n)
